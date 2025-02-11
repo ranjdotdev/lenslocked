fb5ec5296e72b1d1d922f9510474f3b766e13010
Ranj <ranjdotdev@gmail.com>
2025-02-11 17:02:46 +0300

    Creating a Contact Page

--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 	"net/http"
 )
 
-func handleFunc (w http.ResponseWriter, r *http.Request) {
+func homeHandler (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 	fmt.Fprint(w, "<h1>Hello World</h1>")
-} 
+	} 
+	func contactHandler (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ranjdotdev@gmail.com\">ranjdotdev@gmail.com</a></p>")
+}
+
 
 func main() {
 
-	http.HandleFunc("/", handleFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
     fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
