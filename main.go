96413a2ea7e70e700213de9cdff72741133ef9bc
Ranj <ranjdotdev@gmail.com>
2025-02-11 17:37:52 +0300

    Manual path handling

--- main.go
+++ main.go
@@ -14,11 +14,21 @@ func homeHandler (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ranjdotdev@gmail.com\">ranjdotdev@gmail.com</a></p>")
 }
 
+func pathHandler (w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		fmt.Fprintf(w, "<h1>404 Page Not Found</h1>")
+	}
+	fmt.Fprintf(w, "<p>You visited: \".%s\"</p>", r.URL.Path)
+}
 
 func main() {
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
     fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
