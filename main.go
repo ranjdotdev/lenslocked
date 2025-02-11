49ce597e556ed90b69e58a8506cb6ec090bf4fc3
Ranj <ranjdotdev@gmail.com>
2025-02-12 00:04:44 +0300

    Chiii

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler (w http.ResponseWriter, r *http.Request) {
@@ -14,21 +16,10 @@ func homeHandler (w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ranjdotdev@gmail.com\">ranjdotdev@gmail.com</a></p>")
 }
 
-func pathHandler (w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		fmt.Fprintf(w, "<h1>404 Page Not Found</h1>")
-	}
-	fmt.Fprintf(w, "<p>You visited: \".%s\"</p>", r.URL.Path)
-}
-
 func main() {
-
-	http.HandleFunc("/", pathHandler)
+	r := chi.NewRouter();
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
     fmt.Println("Server is running on port 3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
